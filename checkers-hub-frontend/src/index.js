fd9674a6afa4b5b6db45a07ec276f0b04f29b5ac
ianmcknnn <ianmcnnn@gmail.com>
2020-07-27 22:37:51 -0400

    started work on generating pieces

--- checkers-hub-frontend/src/index.js
+++ checkers-hub-frontend/src/index.js
@@ -1,6 +1,7 @@
+const boardDiv = document.getElementById('board');
+
 function generateBoard() {
 	const boardTable = document.createElement('table');
-	const boardDiv = document.getElementById('board');
 
 	for(let i=0; i < 8; i++){
 		const newRow = document.createElement('tr')
@@ -21,5 +22,21 @@ function generateBoard() {
 	}
 	boardDiv.append(boardTable);
 }
+
+function placePieces(){
+
+	for(let i = 0; i < 3; i++){
+		for(let j = 0; j < 8; j++){
+			if((i+j) % 2 == 0){
+
+			}
+		}
+	}
+}
+
+function board(i, j){
+	return document.querySelector(`tr.row-${i}`);
+}
+console.log(board(1,3));
 
 generateBoard();
