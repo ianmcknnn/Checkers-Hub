d62098e41cf598a5fd5236506f39ab6cd9bd6189
ianmcknnn <ianmcnnn@gmail.com>
2020-07-28 15:57:28 -0400

    ran migration for new resources

---
+++ checkers-hub-backend/db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_28_184619) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_games", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.string "moves"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_user_games_on_game_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "bio"
+    t.string "favorite_game"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "user_games", "games"
+end
